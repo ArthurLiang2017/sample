9fae5d99814aabc8b6c8012941210cde3c424cb0
ArthurLiang2017 <arthur_liang@qq.com>
2017-01-18 03:31:48 +0000

    Add password resets & email configuration

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,11 @@ Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
 
+Route::get('password/email', 'Auth\ResetPasswordController@getEmail')->name('password.reset');
+Route::post('password/email', 'Auth\ResetPasswordController@postEmail')->name('password.reset');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@getReset')->name('password.edit');
+Route::post('password/reset', 'Auth\ResetPasswordController@postReset')->name('password.update');
+
 /*Route::get('/', function () {
     return view('welcome');
 });*/
